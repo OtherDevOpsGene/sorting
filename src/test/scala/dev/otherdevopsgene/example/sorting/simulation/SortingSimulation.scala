da46a2437a054e8dda8011919dadce4500f64ece
Gene Gotimer <otherdevopsgene@portinfo.com>
2024-06-09 10:45:23 -0400

    updated Scala scenario

--- src/test/scala/dev/otherdevopsgene/example/sorting/simulation/SortingSimulation.scala
+++ src/test/scala/dev/otherdevopsgene/example/sorting/simulation/SortingSimulation.scala
@@ -3,18 +3,9 @@ package dev.otherdevopsgene.example.sorting.simulation
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class RecordedSimulation extends Simulation {
+class SortingSimulation extends Simulation {
 
-  val httpProtocol = http
-    .baseUrl("http://localhost:8080")
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .upgradeInsecureRequestsHeader("1")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0")
-
-  val scn = scenario("RecordedSimulation")
-    .exec(http("Homepage")
+  val sorts = exec(http("Homepage")
       .get("/"))
     .pause(1)
     .repeat(10) {
@@ -40,5 +31,18 @@ class RecordedSimulation extends Simulation {
           .get("/words?sort=java"))
     }
 
-  setUp(scn.inject(rampUsers(2) during (10)).protocols(httpProtocol))
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+      .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+      .acceptLanguageHeader("en-US,en;q=0.5")
+      .acceptEncodingHeader("gzip, deflate")
+      .userAgentHeader(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0"
+      )
+
+  val sorting = scenario("SortingSimulation").exec(sorts)
+
+  setUp(
+    sorting.inject(rampUsers(2).during (10))
+  ).protocols(httpProtocol)
 }
